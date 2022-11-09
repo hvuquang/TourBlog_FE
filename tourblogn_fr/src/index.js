c982941784b3e90bfc823f1a51d39c88c60f4d83
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-09 13:23:49 +0700

    Revert "Merge branch 'Truong'"
    This reverts commit 24d12e08dc60540823fa032f46f4571c331113a3, reversing
    changes made to 0646ec9a155bfe117c9eae0a22dccb43075f1deb.

--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -4,12 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import LogIn from "./components/Screen/LogIn/LogIn";
-import Register from "./components/Screen/Register/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Register/>
+    <LogIn />
   </React.StrictMode>
 );
 
