a1887e093641c8756c25986850b7400de2d18488
DuyTruong-080 <113657419+DuyTruong-237@users.noreply.github.com>
2022-11-09 10:48:47 +0700

    dsg Button

---
+++ tourblogn_fr/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Button.css'
+
+export default class Button extends Component {
+  render() {
+    return (
+      <center><button className="mainbutton">{this.props.children}</button></center>
+    )
+  }
+}
--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from './components/Button/Button';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Button>login</Button>
 );
 
 // If you want to start measuring performance in your app, pass a function
