9b8ff8eceabae0dffb1383355e88473d1f715e88
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-09 11:03:35 +0700

    update #2
    - inputtext

--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TextInput from './components/TextInput/TextInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TextInput name="UserName"/>
   </React.StrictMode>
 );
 
