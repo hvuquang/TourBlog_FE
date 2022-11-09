722887669e0175b9023fb556ba26f0e3c8d243a8
DuyTruong-080 <113657419+DuyTruong-237@users.noreply.github.com>
2022-11-09 11:11:48 +0700

    component background

--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Button from './components/Button/Button';
+import Background from './components/background/background';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Button>login</Button>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
