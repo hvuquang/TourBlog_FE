f63caf7ca40f44b303531e98a0eddf95d3ad72c6
DuyTruong-080 <113657419+DuyTruong-237@users.noreply.github.com>
2022-11-09 13:10:37 +0700

    update #3
    - register

--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import LogIn from "./components/Screen/LogIn/LogIn";
+import Register from "./components/Screen/Register/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LogIn />
+    <Register/>
   </React.StrictMode>
 );
 
