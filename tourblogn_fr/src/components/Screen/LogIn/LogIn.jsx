5c39a4dab94ffbfe4b7e81abd01bcb6d27c0f0d1
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-09 13:24:37 +0700

    Revert "Merge remote-tracking branch 'origin/HuyV' into Truong"
    This reverts commit 6834cdbf1c90fa6d2b6f3a3aeadcdf1c75e62cd2, reversing
    changes made to 49696d2cbcae28406154feaa41a260f2c057d948.

--- tourblogn_fr/src/components/Screen/LogIn/LogIn.jsx
+++ tourblogn_fr/src/components/Screen/LogIn/LogIn.jsx
@@ -7,29 +7,23 @@ import "./LogIn.css";
 class LogIn extends Component {
   render() {
     return (
-      <div className="background-container">
-        <BackGround>
-          <div class="background__input">
-            <TextInput
-              name="UserName"
-              link="/images/username-removebg-preview.png"
-              input="text"
-            />
-          </div>
-          <div id="background__input">
-            <TextInput
-              name="Password"
-              link="/images/password-removebg-preview.png"
-              input="password"
-            />
-          </div>
-          <div className="link--password">
-            <Link content="Forgot password?" />
-          </div>
-          <Button>LOGIN</Button>
-          <Link content="Register ?" />
-        </BackGround>
-      </div>
+      <BackGround>
+        <TextInput
+          name="UserName"
+          link="/images/username-removebg-preview.png"
+          input="text"
+        />
+        <TextInput
+          name="Password"
+          link="/images/password-removebg-preview.png"
+          input="password"
+        />
+        <div className="link--password">
+          <Link content="Forgot password?" />
+        </div>
+        <Button>LOGIN</Button>
+        <Link content="Register ?" />
+      </BackGround>
     );
   }
 }
