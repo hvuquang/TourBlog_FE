5c46171e30ee681fe0d248ac407ea2b947a0c6c6
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-16 13:20:10 +0700

    update#5
    - change CSS selector

--- tourblogn_fr/src/components/InputNoneIMG/InputNoneIMG.jsx
+++ tourblogn_fr/src/components/InputNoneIMG/InputNoneIMG.jsx
@@ -6,10 +6,9 @@ import { IconContext } from "react-icons";
 class TextInput extends Component {
   render() {
     return (
-      <div className="container">
-        
+      <div className="InputNoneIMG__container">
         <input
-          className="container__input"
+          className="InputNoneIMG__container__input"
           type={this.props.input}
           placeholder={this.props.name}
         />
