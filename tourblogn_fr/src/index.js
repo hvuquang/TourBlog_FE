dae8bbdfa56d3c7a08ff1c2b3ae0e521af07de9c
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-11-30 15:29:50 +0700

    update

--- tourblogn_fr/src/index.js
+++ tourblogn_fr/src/index.js
@@ -8,17 +8,22 @@ import Register from "./components/Screen/Register/Register";
 import ForgetPassword from "./components/Screen/ForgetPassword/ForgetPassword";
 /*import Card from "./components/Card/Card";*/
 import Profile from "./components/Screen/Profile/Profile";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-import AddPost from './components/AddPost/AddPost'
-import Post from './components/Post/Post'
+import AddPost from "./components/AddPost/AddPost";
+import Post from "./components/Post/Post";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}><LogIn/></Provider>
+  <Router>
+    <Provider store={store}>
+      <LogIn />
+    </Provider>
+    {/* <Profile /> */}
+  </Router>
   // <Post />
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
