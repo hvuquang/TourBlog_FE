0dc15404d597910cfd489a0981c2c0528b5dcaa5
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-12-07 16:09:43 +0700

    update

--- tourblogn_fr/src/components/Screen/Header/Header.jsx
+++ tourblogn_fr/src/components/Screen/Header/Header.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export default function Header() {
   return (
     <div className='Header'>
-        <img className='Header_Logo' src={process.env.PUBLIC_URL + '/images/logoTravel.png'} alt='logo'/>
+        <Link to={'/'}><img className='Header_Logo' src={process.env.PUBLIC_URL + '/images/logoTravel.png'} alt='logo'/></Link>
         <div className='Header_Profile'>
             <h3 className='Header_NameProfile'>Vũ Quang Huy</h3>
             <Link to={'/profile'}><img className='Header_icProfile' src={process.env.PUBLIC_URL + '/images/username-removebg-preview.png'} alt='Header_icProfile'/></Link>
